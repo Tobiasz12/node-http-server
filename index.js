0cd83d107ba193ca05860a922a3421e7d645711f
Tobiasz12 <tobiasz63@op.pl>
2017-05-13 20:59:46 +0200

    task_1.8

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ var http = require('http');
 var fs = require('fs');
 var server = http.createServer();
 
-fs.readFile('error.jpg', function(err, data) {
-	server.on('request', function (request, response) {
-	    if (request.method === 'GET' && request.headers.host === 'localhost:9001') {
-	    	response.setHeader("Content-Type", "text/html; charset=utf-8");
-	        response.write('<h1>Hello World!</h1>');
-	        response.end();
-	    } else {
-	    	if (err) throw err;
-	        response.statusCode = 404;
+server.on('request', function (request, response) {
+    if (response.statusCode === 404) {
+    	fs.readFile('error.jpg', function(err, data) {
+    		if (err) throw err; 
 	        response.setHeader("Content-Type", "image/jpeg");
 	        response.end(data);
-	    }
-	});
-	console.log('Server running at http://localhost:9000/');
+        });	     
+   	} else {
+	    response.setHeader("Content-Type", "text/html; charset=utf-8");
+	    response.write('<h1>Hello World</h1>');
+	    response.end();     
+    }
+   
+   
 });
+console.log('Server running at http://localhost:9000/');
+
 server.listen(9000);
