5499cdcf5c8ddf2a4717e675d68e828120113989
Tobiasz12 <tobiasz63@op.pl>
2017-05-13 20:15:40 +0200

    Task 1.8

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var fs = require('fs');
 var server = http.createServer();
 
 fs.readFile('error.jpg', function(err, data) {
-	if (err) throw err;
 	server.on('request', function (request, response) {
 	    if (request.method === 'GET' && request.headers.host === 'localhost:9001') {
 	    	response.setHeader("Content-Type", "text/html; charset=utf-8");
 	        response.write('<h1>Hello World!</h1>');
 	        response.end();
 	    } else {
+	    	if (err) throw err;
 	        response.statusCode = 404;
 	        response.setHeader("Content-Type", "image/jpeg");
 	        response.end(data);
